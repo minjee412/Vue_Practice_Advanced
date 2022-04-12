6d76a52931640fe6758f8ffbbb5e84a038f75058
minjee412 <minjee412@gmail.com>
2022-04-12 20:20:55 +0900

    라우터트랜지션

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,36 @@
     <tool-bar></tool-bar>
     <!--- 🥞 해당 태그로 이동 한다. --->
 
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-
 import ToolBar from './components/ToolBar.vue';
 
 export default {
   components: {
     ToolBar,
-
-  }
-}
+  },
+};
 </script>
 
 <style>
+body {
+  padding: 0;
+  margin: 0;
+}
+
+/*  Router Transition  */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+
+.page-endter,
+.page-leave-to {
+  opacity: 0;
+}
 </style>
