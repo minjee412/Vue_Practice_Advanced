12475ab3bfacc569c9541185c165b074ba2770b9
minjee412 <minjee412@gmail.com>
2022-04-25 13:07:54 +0900

    2022.04.25

--- src/api/index.js
+++ src/api/index.js
@@ -8,16 +8,26 @@ function fetchNewsList() {
   return axios.get(`${config.baseurl}news/1.json`);
 }
 
-function fetchAskList() {
-  return axios.get(`${config.baseurl}ask/1.json`);
+async function fetchAskList() {
+  try {
+    const response = await axios.get(`${config.baseurl}ask/1.json`);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function fetchJobsList() {
   return axios.get(`${config.baseurl}jobs/1.json`);
 }
 
-function fetchList(pageName) {
-  return axios.get(`${config.baseurl}${pageName}/1.json`);
+async function fetchList(pageName) {
+  try {
+    const response = await axios.get(`${config.baseurl}${pageName}/1.json`);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function fetchUserInfo(username) {
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,36 +1,88 @@
-import { fetchUserInfo, fetchItemInfo, fetchList } from '../api';
+import {
+  fetchUserInfo,
+  fetchItemInfo,
+  fetchList,
+  fetchNewsList,
+  fetchJobsList,
+  fetchAskList,
+} from '../api';
 
 export default {
-  FETCH_USER({ commit }, name) {
-    return fetchUserInfo(name)
-      .then(({ data }) => {
-        commit('SET_USER', data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  // ✔️ promise
+  // FETCH_NEWS(context) {
+  //   return fetchNewsList()
+  //     .then((response) => {
+  //       context.commit('SET_NEWS', response.data);
+  //       return response;
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // },
+
+  // 👩‍💻 async
+  async FETCH_NEWS(context) {
+    const response = await fetchNewsList();
+    context.commit('SET_NEWS', response.data);
+    return response;
   },
 
-  FETCH_ITEM({ commit }, item) {
-    return fetchItemInfo(item)
-      .then(({ data }) => {
-        commit('SET_ITEM', data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  // ✔️ promise
+  // FETCH_JOBS({ commit }) {
+  //   return fetchJobsList({ commit })
+  //     .then(({ data }) => {
+  //       commit('SET_JOBS', data);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // },
+
+  // 👩‍💻 async
+  async FETCH_JOBS({ commit }) {
+    try {
+      const response = await fetchJobsList({ commit });
+      commit('SET_JOBS', response.data);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   },
 
-  // #2
-  FETCH_LIST({ commit }, pageName) {
-    // #3
-    return fetchList(pageName)
-      .then((response) => {
-        // #4
-        console.log(4);
-        commit('SET_LIST', response.data);
-        return response;
-      })
-      .catch((error) => console.log(error));
+  // 👩‍💻 async
+  async FETCH_ASK({ commit }) {
+    // 🍥 fetchAskList쪽에서 try catch 처리 하면 따로
+    // 해당 부분에서 try catch 처리 할 필요가 없다. 🍥
+    const response = await fetchAskList({ commit });
+    commit('SET_ASK', response.data);
+    return response;
+  },
+
+  // 👩‍💻 async
+  async FETCH_USER({ commit }, name) {
+    // ✔️ promise
+    // return fetchUserInfo(name)
+    //   .then(({ data }) => {
+    //     commit('SET_USER', data);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    const response = await fetchUserInfo(name);
+    commit('SET_USER', response.data);
+    return response;
+  },
+
+  async FETCH_ITEM({ commit }, item) {
+    const response = await fetchItemInfo(item);
+    commit('SET_ITEM', response.data);
+    return response;
+  },
+
+  async FETCH_LIST({ commit }, pageName) {
+    const response = await fetchList(pageName);
+    commit('SET_LIST', response.data);
+    return response;
   },
 };
